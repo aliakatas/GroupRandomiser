3fa591a1670f8fbcc6ceb16ac1fbe8c7c35421b7
Aristotelis.Liakatas <Aristotelis.Liakatas@jacobs.com>
2020-11-17 15:10:33 +0000

    Creates UI with all core functionality. Some error trapping is still outstanding.

--- gui_main.py
+++ gui_main.py
@@ -1,6 +1,5 @@
-# Building with help from https://build-system.fman.io/pyqt5-tutorial
-
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QCheckBox
+from PyQt5.QtWidgets import QApplication
+from randomiserGUI import Randomiser
 
 import sys 
 
@@ -8,66 +7,8 @@ if __name__ == "__main__":
     
     # General needs
     app = QApplication([])
-    window = QWidget()
-    openFileWidget = QWidget()
-    saveFileWidget = QWidget()
-    optionsWidget = QWidget()
-    execWidget = QWidget()
-
-    # Get the correct layout - top-level
-    windowLayout = QVBoxLayout()
-    openLayout = QHBoxLayout()
-    saveToLayout = QHBoxLayout()
-    optionsLayout = QHBoxLayout()
-    execLayout = QHBoxLayout()
-
-    # Create Buttons
-    browseOpenButton = QPushButton('Browse')
-    browseSaveToButton = QPushButton('Browse')
-    runButton = QPushButton('Run!')
-    closeButton = QPushButton('Quit')
-
-    # Create data entry fields
-    openFileEntry = QLineEdit()
-    saveFileEntry = QLineEdit()
-
-    # Create options entry
-    sizeOfGroupEntry = QLineEdit()
-    allowLessInGroupOpt = QCheckBox('Allow less in group')
-
-    # Upper block
-    openLayout.addWidget(QLabel('Name list:'))
-    openLayout.addWidget(openFileEntry)
-    openLayout.addWidget(browseOpenButton)
-    openFileWidget.setLayout(openLayout)
-    openFileWidget.show()
-
-    # Mid block 
-    saveToLayout.addWidget(QLabel('Save groups to:'))
-    saveToLayout.addWidget(saveFileEntry)
-    saveToLayout.addWidget(browseSaveToButton)
-    saveFileWidget.setLayout(saveToLayout)
-    saveFileWidget.show()
-
-    # Options block
-    optionsLayout.addWidget(QLabel('Group size:'))
-    optionsLayout.addWidget(sizeOfGroupEntry)
-    optionsLayout.addWidget(allowLessInGroupOpt)
-    optionsWidget.setLayout(optionsLayout)
-    optionsWidget.show()
-
-    # Lower block
-    execLayout.addWidget(runButton)
-    execLayout.addWidget(closeButton)
-    execWidget.setLayout(execLayout)
-    execWidget.show()
-
-    # Sort out a few more bits...
-    windowLayout.addWidget(openFileWidget)
-    windowLayout.addWidget(saveFileWidget)
-    windowLayout.addWidget(optionsWidget)
-    windowLayout.addWidget(execWidget)
-    window.setLayout(windowLayout)
+    window = Randomiser()
+    
     window.setWindowTitle('Group Randomiser')
     window.show()
 
---
+++ randomiserGUI.py
@@ -0,0 +1,150 @@
+# Building with help from https://build-system.fman.io/pyqt5-tutorial
+
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QCheckBox, QFileDialog, QMessageBox
+import os
+import functions as fns
+import datetime
+
+class Randomiser(QWidget):
+    def __init__(self, parent=None):
+        super(Randomiser, self).__init__(parent)
+
+        self.userhome = os.path.expanduser("~")
+
+        openFileWidget = QWidget()
+        saveFileWidget = QWidget()
+        optionsWidget = QWidget()
+        execWidget = QWidget()
+
+        # Get the correct layout - top-level
+        windowLayout = QVBoxLayout()
+        openLayout = QHBoxLayout()
+        saveToLayout = QHBoxLayout()
+        optionsLayout = QHBoxLayout()
+        execLayout = QHBoxLayout()
+
+        # Create Buttons
+        browseOpenButton = QPushButton('Browse')
+        browseOpenButton.clicked.connect(self.openFileDialog)
+
+        browseSaveToButton = QPushButton('Browse')
+        browseSaveToButton.clicked.connect(self.saveFileDialog)
+        
+        runButton = QPushButton('Run')
+        runButton.clicked.connect(self.createList)
+        
+        closeButton = QPushButton('Quit')
+        closeButton.clicked.connect(self.closeMe)
+
+        # Create data entry fields
+        self.openFileEntry = QLineEdit()
+        self.saveFileEntry = QLineEdit()
+        self.saveFileEntry.setText(os.path.join(self.userhome, 'mygroup.csv'))
+
+        # Create options entry
+        self.sizeOfGroupEntry = QLineEdit()
+        self.sizeOfGroupEntry.setText('2')
+        self.allowLessInGroupOpt = QCheckBox('Allow less in group')
+
+        # Upper block
+        openLayout.addWidget(QLabel('Name list:'))
+        openLayout.addWidget(self.openFileEntry)
+        openLayout.addWidget(browseOpenButton)
+        openFileWidget.setLayout(openLayout)
+        openFileWidget.show()
+
+        # Mid block 
+        saveToLayout.addWidget(QLabel('Save groups to:'))
+        saveToLayout.addWidget(self.saveFileEntry)
+        saveToLayout.addWidget(browseSaveToButton)
+        saveFileWidget.setLayout(saveToLayout)
+        saveFileWidget.show()
+
+        # Options block
+        optionsLayout.addWidget(QLabel('Group size:'))
+        optionsLayout.addWidget(self.sizeOfGroupEntry)
+        optionsLayout.addWidget(self.allowLessInGroupOpt)
+        optionsWidget.setLayout(optionsLayout)
+        optionsWidget.show()
+
+        # Lower block
+        execLayout.addWidget(runButton)
+        execLayout.addWidget(closeButton)
+        execWidget.setLayout(execLayout)
+        execWidget.show()
+
+        # Sort out a few more bits...
+        windowLayout.addWidget(openFileWidget)
+        windowLayout.addWidget(saveFileWidget)
+        windowLayout.addWidget(optionsWidget)
+        windowLayout.addWidget(execWidget)
+        self.setLayout(windowLayout)
+
+    #################################
+    def closeMe(self):
+        self.close()
+    
+    #################################
+    def openFileDialog(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', self.userhome, "CSV files (*.csv *.*)")
+        if len(str(fname[0])) > 0:
+            self.openFileEntry.setText(str(fname[0]))
+
+    #################################
+    def saveFileDialog(self):
+        fname = QFileDialog.getSaveFileName(self, 'Save file', self.userhome, "CSV files (*.csv)")
+        if len(str(fname[0])) > 0:
+            self.saveFileEntry.setText(str(fname[0]))
+
+    #################################
+    def createList(self):
+        msg = QMessageBox()
+
+        dataFile = self.openFileEntry.text()
+        if len(dataFile.strip()) == 0:
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText(f'Source file entry is empty!')
+            msg.setWindowTitle('Failed')
+            msg.setDetailedText('Enter a valid file name and try again.')
+            msg.exec_()
+            return 
+        elif not os.path.exists(dataFile):    
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText(f'{dataFile} does not exist!')
+            msg.setWindowTitle('Failed')
+            msg.setDetailedText('Enter a valid file name and try again.')
+            msg.exec_()
+            return 
+
+        groupFile = self.saveFileEntry.text()
+        if len(groupFile.strip()) == 0:
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText('Target file entry is empty!')
+            msg.setWindowTitle('Failed')
+            msg.setDetailedText('Enter a valid file name and try again.')
+            msg.exec_()
+            return 
+        
+        groupSizeRaw = self.sizeOfGroupEntry.text()
+        if not groupSizeRaw.isnumeric():
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText('Group size must be a number!')
+            msg.setWindowTitle('Failed')
+            msg.setDetailedText('Enter a valid number and try again.')
+            msg.exec_()
+            return 
+
+        groupSize = int(groupSizeRaw)
+        allowLess = self.allowLessInGroupOpt.isChecked()
+        
+        # Do the work
+        fns.main(dataFile, groupFile, groupSize, allowLess, start=1)
+        fname = os.path.split(groupFile)
+        now = datetime.datetime.now()
+        
+        msg.setIcon(QMessageBox.Information)
+        msg.setText(f'{fname[1]} was created')
+        msg.setWindowTitle('Success')
+        msg.setDetailedText(f'Operation completed at {now.strftime("%Y-%m-%d %H:%M:%S")} \n\nRead from {dataFile} \n\nWrite to {groupFile}')
+        msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        msg.exec_()
