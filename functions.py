3268c7d3562ae23cf5a91eba020d3552d681d913
Aristotelis.Liakatas <Aristotelis.Liakatas@jacobs.com>
2020-11-17 11:20:13 +0000

    Organises the functions in a separate unit. Clean-up main.

---
+++ functions.py
@@ -0,0 +1,62 @@
+import random
+
+####################################
+def read_list_from_csv(fname, start=1):
+    with open(fname, 'r') as f:
+        lines = f.readlines()
+    
+    print(lines[start:])
+    out = []
+    for line in lines[start:]:
+        content = line.split(',')
+        out.append(content[0].strip())
+    
+    return out
+
+####################################
+def create_random_groups(names, group_size=2, allow_less=False):
+    n = len(names)
+    groups = []
+
+    while n >= group_size:
+        group = []
+        for i in range(group_size):
+            name = random.choice(names)
+            group.append(name)
+            names.remove(name)
+        
+        groups.append(group)
+        n = len(names)
+    
+    if allow_less:
+        groups.append(names)
+    else:
+        n_rem = len(names)
+        for i in range(n_rem):
+            group = random.choice(groups)
+            group.append(names[i])
+    
+    return groups
+
+####################################
+def show_one_group(group, id):
+    print(f'Group {id}')
+    print('------------')
+    for item in group:
+        print(f'{item} ', end='')
+    print('')
+
+####################################
+def show_all_groups(groups):
+    print('+++++++++++++++++++++++++++++++++')
+    for i, group in zip(range(len(groups)), groups):
+        show_one_group(group, i)
+    print('_________________________________')
+
+####################################
+def save_to_csv(fname, groups):
+    n = len(groups)
+    with open(fname, 'w') as w:
+        for i, group in zip(range(n), groups):
+            names = ', '.join(group)
+            w.write(f'Group {i + 1}, {names} \n')
--- main.py
+++ main.py
@@ -1,66 +1,4 @@
-import os 
-import random
-
-####################################
-def read_list_from_csv(fname, start=1):
-    with open(fname, 'r') as f:
-        lines = f.readlines()
-    
-    print(lines[start:])
-    out = []
-    for line in lines[start:]:
-        content = line.split(',')
-        out.append(content[0].strip())
-    
-    return out
-
-####################################
-def create_random_groups(names, group_size=2, allow_less=False):
-    n = len(names)
-    groups = []
-
-    while n >= group_size:
-        group = []
-        for i in range(group_size):
-            name = random.choice(names)
-            group.append(name)
-            names.remove(name)
-        
-        groups.append(group)
-        n = len(names)
-    
-    if allow_less:
-        groups.append(names)
-    else:
-        n_rem = len(names)
-        for i in range(n_rem):
-            group = random.choice(groups)
-            group.append(names[i])
-    
-    return groups
-
-####################################
-def show_one_group(group, id):
-    print(f'Group {id}')
-    print('------------')
-    for item in group:
-        print(f'{item} ', end='')
-    print('')
-
-####################################
-def show_all_groups(groups):
-    print('+++++++++++++++++++++++++++++++++')
-    for i, group in zip(range(len(groups)), groups):
-        show_one_group(group, i)
-    print('_________________________________')
-
-####################################
-def save_to_csv(fname, groups):
-    n = len(groups)
-    with open(fname, 'w') as w:
-        for i, group in zip(range(n), groups):
-            names = ', '.join(group)
-            w.write(f'Group {i + 1}, {names} \n')
+import functions as fns
 
 ####################################
 if __name__ == "__main__":
@@ -70,16 +8,16 @@ if __name__ == "__main__":
     groupFile = "./data/groups.csv"
     
     print('Reading list of names...', end='')
-    names = read_list_from_csv(dataFile)
+    names = fns.read_list_from_csv(dataFile)
     print('OK')
 
     print(f'Randomising the groups of {groupSize}...', end='')
-    groups = create_random_groups(names, group_size=groupSize)
+    groups = fns.create_random_groups(names, group_size=groupSize)
     print('OK')
 
     print(f'Writing results to file...', end='')
     # show_all_groups(groups)
-    save_to_csv(groupFile, groups)
+    fns.save_to_csv(groupFile, groups)
     print('OK')
     
     print('Done.\n')
