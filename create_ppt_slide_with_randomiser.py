8a5c537cae1f076f4ada83953d72b05ae3fde9f5
Aristotelis Liakatas <aristotelis.liakatas@jacobs.com>
2023-10-01 18:29:19 +0100

    Adds main and fixes issue with distributing items to groups. Adds ability to update existing presentation.

--- create_ppt_slide_with_randomiser.py
+++ create_ppt_slide_with_randomiser.py
@@ -3,9 +3,6 @@ from tkinter import ttk, filedialog
 import random
 from pptx import Presentation
 
-groups = list()
-slide_txt = ""
-
 ####################################
 def append_to_powerpoint(pptx_file, txt, mod_pptx_file=None):
     # Load the PowerPoint presentation
@@ -19,12 +16,22 @@ def append_to_powerpoint(pptx_file, txt, mod_pptx_file=None):
     title = new_slide.shapes.title
     title.text = "Random Grouping!"
 
-    content = new_slide.placeholders[1]  # Placeholder for content
-    content.text = txt
+    for shape in new_slide.shapes:
+        if not shape.has_text_frame:
+            continue
+        text_frame = shape.text_frame
+        text_frame.clear()  # remove any existing paragraphs, leaving one empty one
+        p = text_frame.paragraphs[0]
+        run = p.add_run()
+        run.text = txt
 
     if mod_pptx_file is not None:
         # Save the modified presentation
         presentation.save(mod_pptx_file)
+        result_table.insert("", "end", values=("Saved to", f"{mod_pptx_file}"))
+    else:
+        presentation.save(pptx_file)
+        result_table.insert("", "end", values=("Saved to", f"{pptx_file}"))
 
 ####################################
 def create_random_groups(n, group_size, allow_less=False):
@@ -47,7 +54,9 @@ def create_random_groups(n, group_size, allow_less=False):
     if allow_less:
         groups.append(names)
     else:
+        ngrp = len(groups)
         for i, name in enumerate(names):
+            i = i % ngrp
             groups[i].append(name)
     
     return groups
@@ -75,17 +84,20 @@ def calculate_results():
 
     # Display the results in the table
     result_table.delete(*result_table.get_children())  # Clear existing rows
+    global slide_txt
+    
     slide_txt = ""
     for idx, group in enumerate(groups):
         result_table.insert("", "end", values=(f"Group {idx + 1} ({len(group)})", ", ".join(group)))
-        slide_txt += f"Group {idx + 1} ({len(group)}) \t\t {'', ''.join(group)}\n"
+        slide_txt += f"Group {idx + 1} ({len(group)}) \t\t {', '.join(group)}\n"
 
 ####################################
 def save():
     file_path = file_path_entry.get()
+
     if file_path:
-        if len(slide_txt) > 0:
-            append_to_powerpoint(file_path, slide_txt)
+        append_to_powerpoint(file_path, slide_txt)
+        
     else:
         result_table.delete(*result_table.get_children())  # Clear existing rows
         result_table.insert("", "end", values=(f"Output file:", "Not selected!"))
@@ -97,51 +109,58 @@ def browse_file():
         file_path_entry.delete(0, tk.END)  # Clear the entry field
         file_path_entry.insert(0, file_path)  # Insert the selected file path
 
-# Create the main application window
-root = tk.Tk()
-root.title("Simple Randomiser")
+#############################################
+if __name__ == '__main__':
+    groups = list()
+    slide_txt = ""
 
-# Create labels and entry widgets for input values
-label1 = tk.Label(root, text="Number of students:")
-label1.pack()
-num_of_students = tk.Entry(root)
-num_of_students.pack()
+    # Create the main application window
+    root = tk.Tk()
+    root.title("Simple Randomiser")
 
-label2 = tk.Label(root, text="Number of students in group:")
-label2.pack()
-num_students_group = tk.Entry(root)
-num_students_group.pack()
+    # Create labels and entry widgets for input values
+    label1 = tk.Label(root, text="Number of students:")
+    label1.pack()
+    num_of_students = tk.Entry(root)
+    num_of_students.pack()
 
-# Create a checkbox for on-off control
-allow_less_in_group = tk.IntVar()  # Variable to hold the checkbox state (0 or 1)
-checkbox = tk.Checkbutton(root, text="Allow less in group", variable=allow_less_in_group)
-checkbox.pack()
+    label2 = tk.Label(root, text="Number of students in group:")
+    label2.pack()
+    num_students_group = tk.Entry(root)
+    num_students_group.pack()
 
-# Create a button to trigger the calculation
-calculate_button = tk.Button(root, text="Calculate", command=calculate_results)
-calculate_button.pack()
+    # Create a checkbox for on-off control
+    allow_less_in_group = tk.IntVar()  # Variable to hold the checkbox state (0 or 1)
+    checkbox = tk.Checkbutton(root, text="Allow less in group", variable=allow_less_in_group)
+    checkbox.pack()
 
-# Create a label and entry field for the file path
-file_path_label = tk.Label(root, text="File Path:")
-file_path_label.pack()
+    # Create a button to trigger the calculation
+    calculate_button = tk.Button(root, text="Calculate", command=calculate_results)
+    calculate_button.pack()
 
-file_path_entry = tk.Entry(root)
-file_path_entry.pack()
+    # Create a label and entry field for the file path
+    file_path_label = tk.Label(root, text="File Path:")
+    file_path_label.pack()
 
-# Create a button to open the file dialog
-browse_button = tk.Button(root, text="Browse", command=browse_file)
-browse_button.pack()
+    file_path_entry = tk.Entry(root)
+    file_path_entry.pack()
 
-# Create a button to trigger saving
-save_button = tk.Button(root, text="Save", command=save)
-save_button.pack()
+    # Create a button to open the file dialog
+    browse_button = tk.Button(root, text="Browse", command=browse_file)
+    browse_button.pack()
 
-# Create a table to display the results
-columns = ("Description", "Result")
-result_table = ttk.Treeview(root, columns=columns, show="headings")
-result_table.heading("Description", text="Description")
-result_table.heading("Result", text="Result")
-result_table.pack()
+    # Create a button to trigger saving
+    save_button = tk.Button(root, text="Save", command=save)
+    save_button.pack()
 
-# Run the application
-root.mainloop()
+    # Create a table to display the results
+    columns = ("Description", "Result")
+    result_table = ttk.Treeview(root, columns=columns, show="headings")
+    result_table.heading("Description", text="Description")
+    result_table.heading("Result", text="Result")
+    result_table.pack()
+
+    # Run the application
+    root.mainloop()
+
+
